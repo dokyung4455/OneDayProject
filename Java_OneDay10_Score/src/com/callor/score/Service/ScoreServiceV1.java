e812b990de42c15a9736dd389a67be55794918ea
dokyung4455 <dokyung4455@naver.com>
2021-04-02 09:23:54 +0900

    OneDay

---
+++ Java_OneDay10_Score/src/com/callor/score/Service/ScoreServiceV1.java
@@ -0,0 +1,20 @@
+package com.callor.score.Service;
+
+import java.util.List;
+import java.util.Scanner;
+
+import com.callor.score.model.ScoreVO;
+
+public class ScoreServiceV1 {
+	
+	protected Scanner scan;
+	protected List<ScoreVO> scoreVO;
+	
+	public void makeScore() {
+		
+		
+		
+	}
+	
+
+}
---
+++ Java_OneDay10_Score/src/com/callor/score/model/ScoreVO.java
@@ -0,0 +1,47 @@
+package com.callor.score.model;
+
+public class ScoreVO {
+	private Integer kor;
+	private Integer eng;
+	private Integer math;
+	private Integer scien;
+	private Integer khis;
+	
+	public Integer getKor() {
+		return kor;
+	}
+	public void setKor(Integer kor) {
+		this.kor = kor;
+	}
+	public Integer getEng() {
+		return eng;
+	}
+	public void setEng(Integer eng) {
+		this.eng = eng;
+	}
+	public Integer getMath() {
+		return math;
+	}
+	public void setMath(Integer math) {
+		this.math = math;
+	}
+	public Integer getScien() {
+		return scien;
+	}
+	public void setScien(Integer scien) {
+		this.scien = scien;
+	}
+	public Integer getKhis() {
+		return khis;
+	}
+	public void setKhis(Integer khis) {
+		this.khis = khis;
+	}
+	
+	
+	
+	
+	
+	
+
+}
